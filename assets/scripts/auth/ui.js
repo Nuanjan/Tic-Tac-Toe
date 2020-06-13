4c8f4de6cfc6597d63c03174f42f17cb6e0c02b3
Nuanjan <Maine09102016>
2020-06-13 12:29:50 -0400

    update game-ui.js update massege response succes and failed.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -63,16 +63,14 @@ const chPwSuccess = function () {
   $('form').trigger('reset')
 
   // Messaging:
-  $('#ch-msg').text('Password changed! ')
-  $('#ch-msg').show().css('text-align', 'center').delay(1800).fadeOut(800)
+  $('#ch-msg').text('Password changed! ').css('text-align', 'center').delay(1800).fadeOut(800)
   $('#ch-msg').removeClass()
   $('#ch-msg').addClass('success')
 }
 
 const chPwFailure = function () {
   // Show a error message
-  $('#ch-msg').text('Password Change Failed, Try agin!')
-  $('#ch-msg').show().css('text-align', 'center').delay(1800).fadeOut(800)
+  $('#ch-msg').text('Password Change Failed, Try agin!').css('text-align', 'center').delay(1800).fadeOut(800)
   $('#ch-msg').removeClass()
   $('#ch-msg').addClass('success')
   $('#change-pw')[0].reset()
